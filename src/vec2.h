ee14b166af1a8c07c2c02e5dc3e3c3c8878f248f
eilume <eilume@protonmail.com>
2022-07-17 17:41:59 +0200

    refactor: Add missing operator overloads to Vec2

--- src/vec2.h
+++ src/vec2.h
@@ -10,11 +10,34 @@ struct Vec2 {
     Vec2(float x, float y) : x(x), y(y) {}
     Vec2(const Vec2& other) : x(other.x), y(other.y) {}
 
-    // TODO: add operator+= and operator*=
-    Vec2 operator+(Vec2 other) { return Vec2(x + other.x, y + other.y); }
-    Vec2 operator+(float other) { return Vec2(x + other, y + other); }
+    inline bool IsZero() const { return x == 0 && y == 0; }
 
-    Vec2 operator*(float other) { return Vec2(x * other, y * other); }
+    bool operator==(Vec2 other) const { return x == other.x && y == other.y; }
+    bool operator!=(Vec2 other) const { return x != other.x || y != other.y; }
+
+    Vec2 operator+(Vec2 other) const { return Vec2(x + other.x, y + other.y); }
+    Vec2 operator+(float other) const { return Vec2(x + other, y + other); }
+    
+    void operator+=(Vec2 other) { x += other.x; y += other.y; }
+    void operator+=(float other) { x += other; y += other; }
+    
+    Vec2 operator-(Vec2 other) const { return Vec2(x - other.x, y - other.y); }
+    Vec2 operator-(float other) const { return Vec2(x - other, y - other); }
+    
+    void operator-=(Vec2 other) { x -= other.x; y -= other.y; }
+    void operator-=(float other) { x -= other; y -= other; }
+
+    Vec2 operator*(Vec2 other) const { return Vec2(x * other.x, y * other.y); }
+    Vec2 operator*(float other) const { return Vec2(x * other, y * other); }
+
+    Vec2 operator*=(Vec2 other) { x *= other.x; y *= other.y; }
+    Vec2 operator*=(float other) { x *= other; y *= other; }
+    
+    Vec2 operator/(Vec2 other) const { return Vec2(x / other.x, y / other.y); }
+    Vec2 operator/(float other) const { return Vec2(x / other, y / other); }
+
+    Vec2 operator/=(Vec2 other) { x /= other.x; y /= other.y; }
+    Vec2 operator/=(float other) { x /= other; y /= other; }
 
     float Magnitude() { return sqrt(x * x + y * y); }
 };
@@ -26,6 +49,12 @@ struct BVec2 {
     BVec2() : x(false), y(false) {}
     BVec2(bool x, bool y) : x(x), y(y) {}
 
-    bool Or() { return x || y; }
-    bool And() { return x && y; }
+    inline bool Or() const { return x || y; }
+    inline bool And() const { return x && y; }
+    
+    inline bool True() const { return And(); }
+    inline bool False() const { return !And(); }
+
+    bool operator==(Vec2 other) const { return x == other.x && y == other.y; }
+    bool operator!=(Vec2 other) const { return x != other.x || y != other.y; }
 };
